8840f7917977df99a9b03c1a991b10fb7a314a73
Silvia García Bouza <silvia.gar.bou.dev@gmail.com>
2023-02-26 19:48:04 +0100

    proyecto con recicler view

--- app/src/main/java/com/example/rickandmorty/RickAndMortyAdapter.kt
+++ app/src/main/java/com/example/rickandmorty/RickAndMortyAdapter.kt
@@ -8,5 +8,31 @@ import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.rickandmorty.databinding.ListItemBinding
 
+
 class RickAndMortyAdapter(val listItemsRickAndMorty: List<ItemRickAndMorty>): RecyclerView.Adapter<RickAndMortyAdapter.RickAndMortyHolder>() {
+
+    class RickAndMortyHolder(val view: View): RecyclerView.ViewHolder(view) {
+        val binding = ListItemBinding.bind(view)
+
+        fun render(item: ItemRickAndMorty) {
+            binding.tvNameRickAndMorty.text = item.name
+
+        }
+    }
+
+    override fun getItemCount(): Int = listItemsRickAndMorty.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RickAndMortyHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return RickAndMortyHolder(layoutInflater.inflate(R.layout.list_item, parent, false))
+
+    }
+
+    override fun onBindViewHolder(holder: RickAndMortyHolder, position: Int) {
+        holder.render(listItemsRickAndMorty[position])
+    }
+
 }
+
+
+
